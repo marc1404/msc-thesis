cb1a862f2d4e68160b7fa447e0abf2d0f082dd00
Marc Vornetran <marc.vornetran@gmail.com>
2018-05-21 20:42:24 +0200

    Log error when failing to insert place

--- beer-scraper/src/insert/insertPlace.js
+++ beer-scraper/src/insert/insertPlace.js
@@ -1,11 +1,21 @@
+import consola from 'consola';
+
 export default async function insertBeer(place, db) {
     const { id, slug, name, type, street, locality, region, country, postalCode, ratingCount, ratingValue, facebookUrl, twitterUrl, websiteUrl, telephone, openingTimes, imageUrl } = place;
 
-    return db`
-        INSERT IGNORE INTO places (
-            id, slug, name, type, street, locality, region, country, postal_code, rating_count, rating_value, facebook_url, twitter_url, website_url, telephone, opening_times, image_url
-        ) VALUES (
-            ${id}, ${slug}, ${name}, ${type}, ${street}, ${locality}, ${region}, ${country}, ${postalCode}, ${ratingCount}, ${ratingValue}, ${facebookUrl}, ${twitterUrl}, ${websiteUrl}, ${telephone}, ${openingTimes}, ${imageUrl}
-        );
-    `;
+    try {
+        return await db`
+            INSERT IGNORE INTO places (
+                id, slug, name, type, street, locality, region, country, postal_code, rating_count, rating_value, facebook_url, twitter_url, website_url, telephone, opening_times, image_url
+            ) VALUES (
+                ${id}, ${slug}, ${name}, ${type}, ${street}, ${locality}, ${region}, ${country}, ${postalCode}, ${ratingCount}, ${ratingValue}, ${facebookUrl}, ${twitterUrl}, ${websiteUrl}, ${telephone}, ${openingTimes}, ${imageUrl}
+            );
+        `;
+    } catch (error) {
+        consola.error(`Failed to insert ${name}!`);
+        consola.error(place);
+        consola.error(error);
+    }
+
+    return null;
 };
