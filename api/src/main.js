51703beedb03ba4a727a27c39202d4a499c5a2fa
Marc Vornetran <marc.vornetran@gmail.com>
2018-06-13 15:46:52 +0200

    Enable CORS in api

--- api/src/main.js
+++ api/src/main.js
@@ -1,8 +1,11 @@
 import { router, get } from 'microrouter';
+import cors from 'micro-cors';
 import beer from './beer';
 import reviews from './reviews';
 
+const enableCors = cors();
+
 export default router(
-    get('/beer/:id/reviews', reviews),
-    get('/beer/:id', beer)
+    get('/beer/:id/reviews', enableCors(reviews)),
+    get('/beer/:id', enableCors(beer))
 );
