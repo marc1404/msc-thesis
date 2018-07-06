9f17601862fdb99ba3e3f1fde22f09f88341f67c
Marc Vornetran <marc.vornetran@gmail.com>
2018-07-06 16:22:32 +0200

    Improve query nn script

--- clustering/query_nn.py
+++ clustering/query_nn.py
@@ -14,13 +14,17 @@ model = keyed_vectors.load(beer_id)
 queries = ['tast', 'palat', 'overal', 'aroma', 'appear', 'pour', 'bottl', 'price', 'color', 'smell']
 
 for query in queries:
-    results = model.most_similar(query)
+    try:
+        results = model.most_similar(query)
 
-    for result in results:
-        neighbor, similarity = result
-        statement = "INSERT INTO beer_nn (beer_id, embedding, query, neighbor, similarity) VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE similarity = %s;"
-        values = (beer_id, embedding, query, neighbor, similarity, similarity)
+        for result in results:
+            neighbor, similarity = result
+            statement = "INSERT INTO beer_nn (beer_id, embedding, query, neighbor, similarity) VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE similarity = %s;"
+            values = (beer_id, embedding, query, neighbor, similarity, similarity)
 
-        cursor.execute(statement, values)
+            cursor.execute(statement, values)
+    except KeyError:
+        print(f'Missing {query} in vocabulary!')
+        pass
 
 mysql_helper.disconnect(cnx, cursor)
